bebe6453e18097278c8970b10a5d807094500f9c
Joseph D. Carpinelli <joseph.d.carpinelli@loopy.codes>
2026-03-01 12:29:50 -0500

    chore: configure pre-releases
    release-as: 0.0.1

--- plumbing/git-set-attr/src/cli.rs
+++ plumbing/git-set-attr/src/cli.rs
@@ -2,5 +2,5 @@ use clap::Parser;
 
 #[derive(Parser)]
 #[command(name = "git set-attr", bin_name = "git set-attr")]
-#[command(author, version, about = "Set Git attributes via code, or from the command-line", long_about = None)]
+#[command(author, version, about = "Set Git attributes via code, or from the command-line.", long_about = None)]
 pub struct Cli {}
