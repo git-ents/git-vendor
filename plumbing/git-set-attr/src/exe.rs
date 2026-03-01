ebaef738f528b50ca80a28662d910f3f096474d0
Joseph D. Carpinelli <joseph.d.carpinelli@loopy.codes>
2026-03-01 13:08:13 -0500

    feat: add executor pattern

--- plumbing/git-set-attr/src/exe.rs
+++ plumbing/git-set-attr/src/exe.rs
@@ -1,5 +1,11 @@
-use crate::cli::Cli;
+use crate::{SetAttr, cli::Cli};
 
-pub fn run(_cli: &Cli) -> Result<(), Box<dyn std::error::Error>> {
+pub fn run(cli: &Cli) -> Result<(), Box<dyn std::error::Error>> {
+    let repo = crate::Repository::open(".")?;
+
+    let attributes: Vec<&str> = cli.attributes.iter().map(|s| s.as_str()).collect();
+
+    repo.set_attr(&cli.pattern, &attributes, cli.file.as_deref())?;
+
     Ok(())
 }
