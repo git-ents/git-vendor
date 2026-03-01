2fa51dc3e651a11f89614c35177a3529f99f3a3b
Joseph D. Carpinelli <joseph.d.carpinelli@loopy.codes>
2026-03-01 12:29:50 -0500

    chore: configure pre-releases
    release-as: 0.0.1

--- src/cli.rs
+++ src/cli.rs
@@ -2,5 +2,5 @@ use clap::Parser;
 
 #[derive(Parser)]
 #[command(name = "git vendor", bin_name = "git vendor")]
-#[command(author, version, about = "An in-source vendoring alternative to Git submodules and subtrees", long_about = None)]
+#[command(author, version, about = "An in-source vendoring alternative to Git submodules and subtrees.", long_about = None)]
 pub struct Cli {}
