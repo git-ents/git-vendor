92052e317bcf6b22e15717272dbbe21eb9247caf
Joseph D. Carpinelli <joseph.carpinelli@icloud.com>
2026-05-17 11:35:19 -0400

    chore: add scaffolding for trait implementations

--- crates/git-vendor/src/lib.rs
+++ crates/git-vendor/src/lib.rs
@@ -5,3 +5,41 @@ mod vendor;
 
 pub use error::Error;
 pub use vendor::{VendorEntry, VendorRepository};
+
+impl VendorRepository for gix::Repository {
+    fn list_vendors(&self) -> Result<Vec<VendorEntry>, Error> {
+        todo!()
+    }
+
+    fn get_vendor(&self, _name: &str) -> Result<Option<VendorEntry>, Error> {
+        todo!()
+    }
+
+    fn add_vendor(&self, _entry: &VendorEntry) -> Result<gix::index::State, Error> {
+        todo!()
+    }
+
+    fn remove_vendor(&self, _name: &str) -> Result<(), Error> {
+        todo!()
+    }
+
+    fn fetch_vendor(&self, _entry: &VendorEntry) -> Result<gix::ObjectId, Error> {
+        todo!()
+    }
+
+    fn track_vendor_pattern(&self, _entry: &VendorEntry) -> Result<(), Error> {
+        todo!()
+    }
+
+    fn refresh_vendor_attrs(&self, _entry: &VendorEntry) -> Result<(), Error> {
+        todo!()
+    }
+
+    fn merge_vendor(&self, _entry: &VendorEntry) -> Result<gix::index::State, Error> {
+        todo!()
+    }
+
+    fn vendor_attributes(&self, _entry: &VendorEntry) -> Result<Vec<String>, Error> {
+        todo!()
+    }
+}
